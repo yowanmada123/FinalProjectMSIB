b9ff66944817cb14b682d247025eec2df4a10420
Yowanda Frisky Amada <yowanmada@gmail.com>
2022-11-21 13:40:36 +0700

    change title

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const GetMaterialApp(
         debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Final Project',
       home: SplashScreenPage(),
     );
   }
